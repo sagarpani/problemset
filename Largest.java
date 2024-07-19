270a7786403ecb25c1674009ef81e81af9a6e81c
sagarpani <sagarpani22@gmail.com>
2024-07-19 21:14:53 +0530

    Add problem2

---
+++ Largest.java
@@ -0,0 +1,17 @@
+//The largest among 3 no.s 
+
+import java.util.Scanner;
+public class Largest {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter 3 no.s :");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            int max = x;
+            if(y>max) max = y;
+            if(z>max) max = z;
+            System.out.println("The largest no. is : "+max);
+        }
+    }
+}
