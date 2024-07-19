c4bb6925481884983132f601c6f3046b344583f4
sagarpani <sagarpani22@gmail.com>
2024-07-19 23:10:28 +0530

    Add problem 4

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.print("The no. of digits you want to print : ");
+        int x = sc.nextInt();
+        if(x==1) System.out.println(0);
+        if(x==2) System.out.println(1);
+        if(x>2) {
+            int a = 0;
+            int b = 1;
+            for (int i = 3; i <= x; i++) {
+                int temp = b;
+                b = a + b;
+                a = temp;
+            }
+        System.out.println(b);
+        sc.close();
+        }
+    }
+}
