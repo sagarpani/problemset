d6cb5140461b334c18196a14576c7053bd445fe3
sagarpani <sagarpani22@gmail.com>
2024-07-19 21:42:24 +0530

    Add problem3

---
+++ Case.java
@@ -0,0 +1,14 @@
+//Input a character and print whether that character is Uppercase or Lowercase letter
+
+import java.util.Scanner;
+public class Case {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a character :");
+            char ch = sc.next().trim().charAt(0);
+            if('a'<=ch && ch<='z') System.out.println("This is a lowercase letter");
+            else if('A'<=ch && ch<='z') System.out.println("This is an uppercase letter");
+            else System.out.println("Invalid input");
+        }
+    }
+}
