eb69be0d9d77b9422d269097b21f51e531cf4fbe
sagarpani <sagarpani22@gmail.com>
2024-08-03 07:01:59 +0530

    Swapping In Array

---
+++ Problem_10.java
@@ -0,0 +1,16 @@
+//Swapping two index values in an array
+
+import java.util.Arrays;
+
+public class Problem_10 {
+    public static void main(String[] args) {
+      int[] arr = {2,3,4,5,6};
+      swap(arr,1,2);
+      System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
