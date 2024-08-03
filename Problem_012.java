8ff9e353198db300cfb413d3c1a6ab6f0ac209a0
sagarpani <sagarpani22@gmail.com>
2024-08-04 00:50:57 +0530

    Reversal Of An Array

---
+++ Problem_012.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Problem_012 {
+     public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
