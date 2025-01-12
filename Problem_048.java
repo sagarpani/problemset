df4ccf7da48fc647a378e204fe3993e8ce8634b5
sagarpani <sagarpani22@gmail.com>
2025-01-12 20:42:05 +0530

    Insertion Sort

--- Problem_048.java
+++ Problem_048.java
@@ -1,28 +1,26 @@
 //INSERTION SORT
 
 import java.util.Arrays;
-public class Problem_048{
+public class Problem_048 {
     public static void main(String[] args) {
-        int[] arr = {7,6,56,3,2};
+        int[] arr = {7, 6, 56, 3, 2};
         insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void insertionSort(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j > 0; j--) {
-                if(arr[j] < arr[j-1]){
-                    swap(arr,j,j-1);
-                }
-                else{
-                    break;
-                }
+
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements of arr[0..i-1] that are greater than key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+
+            // Place key at the correct position
+            arr[j + 1] = key;
         }
     }
-    static void swap(int[] arr, int first, int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
-
 }
